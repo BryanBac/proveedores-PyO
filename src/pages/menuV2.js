e2e8bc63936dbda3357a819ccc664e74608fcf92
BryanBac <bebac@correo.url.edu.gt>
2023-10-22 17:15:36 -0600

    implementación de lo de rodolfo

--- src/pages/menuV2.js
+++ src/pages/menuV2.js
@@ -127,80 +127,6 @@ export default function MiniDrawer(props) {
       return ""
     }
   })
-
-  const ChangeOptionMenu = (id) => {
-    optionMenu = id;
-    if (id === 0) {
-      setOption1(true)
-      setOption2(false)
-      setOption3(false)
-      setOption4(false)
-      setOption5(false)
-      setOption6(false)
-      setOption7(false)
-    } else if (id === 1) {
-      setOption1(false)
-      setOption2(true)
-      setOption3(false)
-      setOption4(false)
-      setOption5(false)
-      setOption6(false)
-      setOption7(false)
-    } else if (id === 2) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(true)
-      setOption4(false)
-      setOption5(false)
-      setOption6(false)
-      setOption7(false)
-    } else if (id === 3) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(false)
-      setOption4(true)
-      setOption5(false)
-      setOption6(false)
-      setOption7(false)
-      sessionStorage.setItem("platilloList", JSON.stringify(orden));
-      sessionStorage.setItem("ordenList", JSON.stringify(orden));
-      sessionStorage.setItem("platilloListGranizadas", JSON.stringify(orden));
-      sessionStorage.setItem("platos", JSON.stringify(orden));
-    } else if (id === 4) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(false)
-      setOption4(false)
-      setOption5(true)
-      setOption6(false)
-      setOption7(false)
-    } else if (id === 5) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(false)
-      setOption4(false)
-      setOption5(false)
-      setOption6(true)
-      setOption7(false)
-    } else if (id === 6) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(false)
-      setOption4(false)
-      setOption5(false)
-      setOption6(false)
-      setOption7(true)
-    } else if (id === 7) {
-      setOption1(false)
-      setOption2(false)
-      setOption3(false)
-      setOption4(false)
-      setOption5(false)
-      setOption6(false)
-      setOption7(false)
-      sessionStorage.setItem("acceso", false)
-    }
-  }
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -417,9 +343,9 @@ export default function MiniDrawer(props) {
     }
   }, [empCant, numEmpanadas])
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(usuario, typeof usuario)
-  },[usuario])
+  }, [usuario])
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -449,82 +375,82 @@ export default function MiniDrawer(props) {
           </IconButton>
         </DrawerHeader>
         <Divider />
-        {usuario == "0" && 
+        {usuario == "0" &&
           <>
             <List>
-            <ListItem key="Fabrica" disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
+              <ListItem key="Fabrica" disablePadding sx={{ display: 'block' }}>
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
                   }}
                 >
-                  <button className={styles.icon} onClick={() => {
-                    router.replace("/fabrica/inicio")
-                  }} ><AccountBoxOutlinedIcon /></button>
-                </ListItemIcon>
-                <ListItemText primary="Fabrica" sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem key="Mayorista" disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <button className={styles.icon} onClick={() => {
+                      router.replace("/fabrica/inicio")
+                    }} ><AccountBoxOutlinedIcon /></button>
+                  </ListItemIcon>
+                  <ListItemText primary="Fabrica" sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem key="Mayorista" disablePadding sx={{ display: 'block' }}>
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
                   }}
                 >
-                  <button className={styles.icon} onClick={() => {
-                    router.replace("/mayorista/mayorista-inicio")
-                  }} ><AccountBoxOutlinedIcon /></button>
-                </ListItemIcon>
-                <ListItemText primary="Mayorista" sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem key="Minorista" disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <button className={styles.icon} onClick={() => {
+                      router.replace("/mayorista/mayorista-inicio")
+                    }} ><AccountBoxOutlinedIcon /></button>
+                  </ListItemIcon>
+                  <ListItemText primary="Mayorista" sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem key="Minorista" disablePadding sx={{ display: 'block' }}>
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
                   }}
                 >
-                  <button className={styles.icon} onClick={() => {
-                    router.replace("/minoristas/minorista-inicio")
-                  }} ><AccountBoxOutlinedIcon /></button>
-                </ListItemIcon>
-                <ListItemText primary="Minorista" sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          </List>
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <button className={styles.icon} onClick={() => {
+                      router.replace("/minoristas/minorista-inicio")
+                    }} ><AccountBoxOutlinedIcon /></button>
+                  </ListItemIcon>
+                  <ListItemText primary="Minorista" sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            </List>
           </>
         }
         {usuario == "1" &&
@@ -607,28 +533,28 @@ export default function MiniDrawer(props) {
         }
         <Divider />
         <List>
-          {['Finanzas'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
+          <ListItem key="Financiera" disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
                 sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
                 }}
               >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index === 0 && <button className={styles.icon} onClick={() => ChangeOptionMenu(index + 6)}><AttachMoneyIcon /></button>}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+                <button className={styles.icon} onClick={() => {
+                  router.replace("/financiera")
+                }} ><AccountBoxOutlinedIcon /></button>
+              </ListItemIcon>
+              <ListItemText primary="Financiera" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
         </List>
         <Divider />
         <List>
